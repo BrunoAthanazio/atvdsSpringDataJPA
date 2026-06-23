7f3728f7cabd5417b0cda0840085d495cfea4ebe
BrunoAthanazio <bruno_ferreira15@estudante.sesisenai.org.br>
2026-06-23 13:43:00 -0300

    feat: exercicio2 model

---
+++ src/main/java/com/weg/atvdsSpringDataJPA/model/exercicio2/Endereco.java
@@ -0,0 +1,34 @@
+package com.weg.atvdsSpringDataJPA.model.exercicio2;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "endereco")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String rua;
+    
+    private int numero;
+
+    @OneToOne
+    @JoinColumn(name = "funcionario_id")
+    private Funcionario funcionario;
+}
